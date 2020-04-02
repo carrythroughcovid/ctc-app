d3b428e126ce96a85cd23d4bbd10c790028a40bc
Jacqui Shadforth <pwn@live.com.au>
2020-04-02 21:10:32 +1100

    Move onPress() to TouchableOpacity, temporarily breaking styles

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import {
   ScrollView,
   StyleSheet,
   View,
   Text,
   SafeAreaView,
-  TouchableOpacity,
-} from 'react-native';
-import { Input, Divider } from 'react-native-elements';
+  TouchableOpacity
+} from "react-native";
+import { Input, Divider } from "react-native-elements";
 
-import CategoryTile from '../components/CategoryTile'
-import { categories } from "../data/categories"
+import CategoryTile from "../components/CategoryTile";
+import { categories } from "../data/categories";
 
 export default function DiscoverScreen({ navigation }) {
   return (
@@ -27,58 +27,58 @@ export default function DiscoverScreen({ navigation }) {
         <Divider style={styles.divider} />
         <Text style={styles.sectionTitle}>TAKE A BROWSE</Text>
         <Text style={styles.textHeading}>Explore Categories</Text>
-        <TouchableOpacity style={styles.categoryContainer}>
+        <View style={styles.categoryContainer}>
           {categories.map(({ name, iconUrl }, i) => (
-            <View
-              key={i}
-              style={styles.categoryTile}
+            <TouchableOpacity
               onPress={() =>
                 navigation.navigate("SearchResults", { category: name })
               }
             >
-              <CategoryTile iconUrl={iconUrl} category={name} />
-            </View>
+              <View key={i} style={styles.categoryTile}>
+                <CategoryTile iconUrl={iconUrl} category={name} />
+              </View>
+            </TouchableOpacity>
           ))}
-        </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 DiscoverScreen.navigationOptions = {
-  header: null,
+  header: null
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   scrollContainer: {
-    padding: 20,
+    padding: 20
   },
   textHeading: {
-    fontSize: 25,
+    fontSize: 25
   },
   sectionTitle: {
-    color: '#9060EB',
+    color: "#9060EB",
     fontWeight: "bold",
     paddingTop: 15,
-    paddingBottom: 5,
+    paddingBottom: 5
   },
   divider: {
     marginTop: 20,
-    marginBottom: 20,
+    marginBottom: 20
   },
   categoryContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding: 3,
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    padding: 3
   },
   categoryTile: {
     paddingTop: 20,
-    width: '48%',
+    width: "48%"
   }
 });
