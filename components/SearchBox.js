0c42970cb4b90c13afbb3d19dfca8a49183e6e70
Jacqui Shadforth <pwn@live.com.au>
2020-04-25 13:28:14 +1000

    Add clear button inside text input field on SRP

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import PropTypes from "prop-types";
 import { connectSearchBox } from "react-instantsearch-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import colours from "../utils/colours";
 
 const SearchBox = ({ currentRefinement, refine }) => (
   <View style={styles.container}>
-    <TextInput
-      style={styles.input}
-      onChangeText={value => refine(value)}
-      value={currentRefinement}
-      placeholder='Search for location or business'
-    />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        onChangeText={value => refine(value)}
+        value={currentRefinement}
+        placeholder='Search for location or business'
+      />
+      <TouchableOpacity onPress={() => refine(null)}>
+        <Icon
+          style={styles.inputIcon}
+          name='times'
+          size={18}
+          color={colours.textUiTertiary}
+        />
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
@@ -23,14 +42,22 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 16,
   },
-  input: {
-    height: 48,
-    padding: 12,
-    fontSize: 16,
+  inputContainer: {
+    flexDirection: "row",
     backgroundColor: "#fff",
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: "#ddd",
+    borderColor: colours.textUiTertiary,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    padding: 12,
+    fontSize: 16,
+  },
+  inputIcon: {
+    marginVertical: 14,
+    marginHorizontal: 14,
   },
 });
 
