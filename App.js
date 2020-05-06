00b237fda49dfb764ad6d28840d9c5334999ad9a
Matt Perrott <matt.perrott@rea-group.com>
2020-05-06 22:47:19 +1000

    remove old app navigator

--- App.js
+++ App.js
@@ -23,7 +23,6 @@ export default function App(props) {
     return (
       <View style={styles.container}>
         {Platform.OS === "ios" && <StatusBar barStyle='default' />}
-        {/* <AppNavigator /> */}
         <MainNavigator />
       </View>
     );
